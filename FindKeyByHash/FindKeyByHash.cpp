c33b43c860ed74f8782caa147463b85d1e50a1cd
Дмитрий Гридин <msdima6@gmail.com>
2021-03-12 16:06:14 +0200

    Add project files.

---
+++ FindKeyByHash/FindKeyByHash.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <string>
+#include "md5.h"
+#include <vector>
+using std::cout;
+using std::string;
+
+int main()
+{
+	std::vector<char> vec(7); 
+	vec.back() = '\0';
+	int count = 0;
+
+	for (size_t i = 96; i <= 126; ++i)
+	{
+		vec[0] = static_cast<char>(i);
+		vec[5] = static_cast<char>(222 - i);
+
+		for (size_t j = 86; j <= 126; ++j)
+		{
+			vec[2] = static_cast<char>(j);
+			vec[3] = static_cast<char>(212 - j); 
+
+			for (size_t k = 83; k <= 126; ++k)
+			{
+				vec[1] = static_cast<char>(k);
+				vec[4] = static_cast<char>(209 - k);
+
+				++count;
+				if (md5(string(vec.data())) == "96c4dda0c4a0b34262b1d91d47056f9e")
+				{
+					cout << vec.data() << "\n";
+					goto wq;
+				}
+			}
+		}
+
+	}
+
+	wq:
+	cout << count;
+}
+
---
+++ KeyGenApriorit/KeyGenApriorit.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <ctime>  
+#include <conio.h>
+
+using std::cout;
+using std::vector;
+using std::string;
+
+void fill_partially_serial_key(vector<char>& serial_key, size_t last_symb_code, size_t sum, size_t first_ind, size_t sec_ind);
+string generate_key();
+
+int main()
+{ 
+	srand(static_cast<unsigned int>(time(nullptr)));
+ 
+	do
+	{
+		cout << generate_key();
+		cout << "\nPress any key to get next serial key...\n";
+
+	} while (_getch());
+}
+
+string generate_key()
+{
+	vector<char> serial_key(7); 
+	int last_valid_symb_code = 126; 
+
+	// serial_key[0] + serial_key[5] should be 222 (0xDE)
+	fill_partially_serial_key(serial_key, last_valid_symb_code, 222, 0, 5);
+
+	// serial_key[2] + serial_key[3] should be 212 (0xD4) 
+	fill_partially_serial_key(serial_key, last_valid_symb_code, 212, 2, 3);
+
+	// serial_key[1] + serial_key[4] should be 754 - 222 - 212 - 6 * 16 - 15 = 209
+	fill_partially_serial_key(serial_key, last_valid_symb_code, 209, 1, 4);
+
+	serial_key.back() = '\0'; 
+	return serial_key.data(); 
+}
+
+void fill_partially_serial_key(vector<char>& serial_key, size_t last_symb_code, size_t sum, size_t first_ind, size_t sec_ind)
+{
+	size_t offset = sum - last_symb_code; 
+	size_t char_code = rand() % (last_symb_code - offset + 1) + offset;
+
+	serial_key[first_ind] = static_cast<char>(char_code);
+	serial_key[sec_ind] = static_cast<char>(sum - char_code);
+}
